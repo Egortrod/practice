eb6680b814f270110cba9aa4db9118b110e6c1a3
Egortrod <egor.arkhipoff@yandex.ru>
2022-11-30 18:15:09 +0300

    add 123456

---
+++ 123456.py
@@ -0,0 +1,9 @@
+print(f'2115616')
+choice = int(input())
+if choice == 1:
+    print(f'12316516')
+    a = int(input())
+    if a == 1:
+        print('1111')
+    elif a ==2:
+        print('2222')
