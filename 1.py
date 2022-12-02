dd1582ec18b3f3a15b685a21986116bfb65f503c
Egortrod <egor.arkhipoff@yandex.ru>
2022-12-02 22:20:30 +0300

    add index, change 1

--- 1.py
+++ 1.py
@@ -1,10 +1,11 @@
 import random
 import time
 start_time = time.time()
-items = [random.randint(0, 10) for i in range(10)]
+items = [random.randint(0, 1000) for i in range(1000)]
 print('Ranomize array:')
 print(items)
 print('Maximum value of array =', max(items))
 print('Minimal value of array =', min(items))
-runtime = (time.time() - start_time) * 100000 
-print('Time passed:' + " %s * 10^-5 seconds " % runtime)
+runtime = (time.time() - start_time) 
+# print('Time passed:' + " %s * 10^-5 seconds " % runtime)
+print(f'Time passed: {round(runtime*1000, 2)} ms') 
