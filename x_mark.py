cc984542f8170a968aead658246b6a150bf8cba9
Egortrod <egor.arkhipoff@yandex.ru>
2023-02-21 15:28:41 +0300

    change x_mark

--- x_mark.py
+++ x_mark.py
@@ -1,21 +1,61 @@
-def x_mark(user_data):
-    source = user_data
+def x_mark_first(user_data):
+    source, idx_n = user_data, user_data
     idx_0 = 0
-    idx_n = user_data
     space = ' '
-    while user_data >= 0:
-        print(int(idx_0) * space + '*' + int(idx_n) * (2 * space) + '*')
+    while user_data > 0:
+        print(idx_0 * space + '*' + idx_n * space * 2 + '*')
         idx_0 += 1
         idx_n -= 1
         user_data -= 1
     while user_data <= source:
-        print(int(idx_0) * space + '*' + int(idx_n) * (2 * space) + '*')
+        print(idx_0 * space + '*' + idx_n * space * 2 + '*')
         idx_0 -= 1
         idx_n += 1
         user_data += 1
-#     while user_data > 0:
-#         print(idx * ' ' + '*' + ' ' * user_data * 3 + '*')
-#         user_data -= 1
-#         idx += 2
-#     print(idx * ' ' + '*')
-x_mark(7)
+    print('\n')
+
+def x_mark_second(user_data):
+    source, idx_n = user_data, user_data
+    idx_0 = 0
+    space = ' '
+    while user_data > 0:
+        print(idx_0 * space + '*' + (2 * idx_n - 1) * space + '*')
+        idx_0 += 1
+        idx_n -= 1
+        user_data -= 1
+    print(idx_0 * space + '*')
+    idx_0 -= 1
+    idx_n += 1
+    user_data += 1
+    while user_data <= source:
+        print(idx_0 * space + '*' + (2 * idx_n - 1) * space + '*')
+        idx_0 -= 1
+        idx_n += 1
+        user_data += 1
+    print('\n')
+
+
+def x_mark_third(user_data):
+    source, idx_n = user_data, user_data
+    idx_0, multiply = 0, 2
+    space = ' '
+    while user_data > 0:
+        print(idx_0 * space * multiply + '*' + (2 * idx_n * multiply - 1) * space + '*')
+        idx_0 += 1
+        idx_n -= 1
+        user_data -= 1
+    print(idx_0 * space * multiply + '*')
+    idx_0 -= 1
+    idx_n += 1
+    user_data += 1
+    while user_data <= source:
+        print(idx_0 * space * multiply + '*' + (2 * idx_n * multiply - 1) * space + '*')
+        idx_0 -= 1
+        idx_n += 1
+        user_data += 1
+    print('\n')
+
+
+x_mark_first(3)
+x_mark_second(3)
+x_mark_third(3)
