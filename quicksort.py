da2eeb20b4e3ccdc47f507ba8e0e73cc3249b612
Egortrod <egor.arkhipoff@yandex.ru>
2023-02-28 16:25:55 +0300

    add quicksort

---
+++ quicksort.py
@@ -0,0 +1,15 @@
+import random
+
+def quicksort(nums):
+   if len(nums) <= 1:
+       return nums
+   else:
+       q = random.choice(nums)
+   l_nums = [n for n in nums if n < q]
+   e_nums = [q] * nums.count(q)
+   b_nums = [n for n in nums if n > q]
+   return quicksort(l_nums) + e_nums + quicksort(b_nums)
+
+data = [1, 8 ,5 ,4 , 7]
+
+print(quicksort(data))
