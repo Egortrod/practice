d38846cbc684f85f419acdc99506bab7535985eb
egortrod <egor.arkhipoff@yandex.ru>
2023-10-26 12:41:25 +0300

    fix

--- quicksort.py
+++ quicksort.py
@@ -2,13 +2,13 @@ from random import randint, choice
 
 def quicksort(nums):
    if len(nums) <= 1:
-       return nums
+        return nums
    else:
-       q = choice(nums)
-   l_nums = [n for n in nums if n < q]
-   e_nums = [q] * nums.count(q)
-   b_nums = [n for n in nums if n > q]
-   return quicksort(l_nums) + e_nums + quicksort(b_nums)
+        q = choice(nums)
+        l_nums = [n for n in nums if n < q]
+        e_nums = [q] * nums.count(q)
+        b_nums = [n for n in nums if n > q]
+        return quicksort(l_nums) + e_nums + quicksort(b_nums)
 
 def qsort(array):
     less = []
@@ -21,7 +21,6 @@ def qsort(array):
                 less.append(x)
             elif x == pivot:
                 equal.append(x)
-                print(equal)
             elif x > pivot:
                 greater.append(x)
         return qsort(less) + equal + qsort(greater)
@@ -31,4 +30,5 @@ def qsort(array):
 
 user_data = [randint(1, 50) for _ in range(20)]
 print(f'user arr: {user_data}')
+print(f'quicksort: {quicksort(user_data)}')
 print(f'sorted arr: {qsort(user_data)}')
